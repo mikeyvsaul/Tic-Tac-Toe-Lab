5f5178e0661e143882bdd711c6d0b16adfed84f9
Michael Saul <michaelsaul@MacBook-Pro.local>
2020-08-02 14:21:20 -0700

    JS Pseudocode

--- main.js
+++ main.js
@@ -1,5 +1,33 @@
 /*----- constants -----*/
+const PLAYERX = 1
+const PLAYERO = -1
+const XWINS = 3
+const OWINS = -3
+
 /*----- app's state (variables) -----*/
+let lineVal; //sum value of a line of 3 cells
+let colArr; //column arrays [1,4,7], [2,5,8], [3,6,9]
+let rowArr; //row arrays [1,2,3], [4,5,6], [7,8,9]
+let diagArr;  //diagonal arrays [1,5,9], [3,5,7]
+
+
 /*----- cached element references -----*/
+//what cells are still empty, what are currently holding an X or O
+
 /*----- event listeners -----*/
+//every other click will put an X or O
+
 /*----- functions -----*/
+init();
+
+function init() {
+  // 
+};
+
+function isGameOver() {
+
+}
+
+function render() {
+
+};

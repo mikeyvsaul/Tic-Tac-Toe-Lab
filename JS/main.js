36987f82ce3bf57c575e0ee500488d356a668500
Michael Saul <michaelsaul@MacBook-Pro.local>
2020-08-06 15:27:21 -0700

    JS write up

--- JS/main.js
+++ JS/main.js
@@ -1,8 +1,42 @@
 /*----- constants -----*/
+const ACTIONS = {
+  '1' :  'red',
+  '-1' : 'blue',
+  null : 'white' 
+};
 
+const WINNINGCOMBOS = [
+  [0, 1, 2]
+  [3, 4, 5]
+  [6, 7, 8]
+  [0, 3, 6]
+  [1, 4, 7]
+  [2, 5, 8]
+  [0, 4, 8]
+  [2, 4, 6]
+];
 /*----- app's state (variables) -----*/
-
+let boardArr;
+let turn;
+let winner;
 
 /*----- cached element references -----*/
+let squares = document.querySelectorAll('box');
+
 /*----- event listeners -----*/
 /*----- functions -----*/
+init();
+
+let init = () => {
+  boardArr = [null, null, null, null, null, null, null, null, null];
+  turn = 1;
+  winner = null;
+  render();
+}
+
+let render = () => {
+  squares.forEach(() => {
+    i = boardArr[i];
+    this.styleMedia.backgroundColor = 
+  })
+}
